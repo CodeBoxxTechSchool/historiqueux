964b4b9e263334e5b7a817b54e4bedcf9d7fa481
Serge Savoie <serge@bokouwi.com>
2014-03-27 09:26:34 -0400

    added unit tests

--- spec/controllers/historiqueux/history_controller_spec.rb
+++ spec/controllers/historiqueux/history_controller_spec.rb
@@ -6,10 +6,20 @@ module Historiqueux
     routes { Historiqueux::Engine.routes }
 
     before(:each) do
+
       @dummy_model = mock_model(DummyModel)
-      @version = double(PaperTrail::Version)
-      @preceding_version = double(PaperTrail::Version)
+      @horses1 = mock_model(Horse)
+      @horses2 = mock_model(Horse)
+      @horses = [@horses1, @horses2]
+
+      @version = mock_model(PaperTrail::Version)
+      @preceding_version = mock_model(PaperTrail::Version)
       @history = [@preceding_version, @version]
+
+      @horse_version = mock_model(PaperTrail::Version, {:item_type => 'Horse'})
+      @horse_preceding_version = mock_model(PaperTrail::Version, {:item_type => 'Horse'})
+      @horses_history = [@horse_preceding_version, @horse_version]
+
     end
 
     describe "GET 'index'" do
@@ -37,6 +47,43 @@ module Historiqueux
 
     end
 
+    describe "GET 'index relations'" do
+
+      it "doit fetché la ressource passé en paramètre et la liste des entités de la relation demandée,
+          le layout de l'application hôte sera utilisé" do
+
+        DummyModel.should_receive(:find).with('1').and_return(@dummy_model)
+        @dummy_model.stub(:versions) { @history }
+
+        @dummy_model.should_receive(:horses).and_return(@horses)
+
+        @horses1.stub(:versions) { @horses_history }
+        @horses2.stub(:versions) { @horses_history }
+
+        get 'index_relations', 'resource' => 'dummy_model', 'resource_id' => '1', :relation_resource => 'horses'
+
+        assert_template layout: 'application'
+
+      end
+
+      it "si l'appel provient d'un call Ajax, on n'affiche pas de layout" do
+
+        DummyModel.should_receive(:find).with('1').and_return(@dummy_model)
+        @dummy_model.stub(:versions) { @history }
+
+        @dummy_model.should_receive(:horses).and_return(@horses)
+
+        @horses1.stub(:versions) { @horses_history }
+        @horses2.stub(:versions) { @horses_history }
+
+        xhr :get, 'index_relations', 'resource' => 'dummy_model', 'resource_id' => '1', :relation_resource => 'horses'
+
+        assert_template layout: false
+
+      end
+
+    end
+
     describe "Comportement du 'show'" do
 
       it "doit fetché les ressource passé en paramètre ainsi que la version de l'historique sélectionné avec sa version
@@ -44,7 +91,7 @@ module Historiqueux
 
         subject.should_receive(:fetch_resource).and_return(@dummy_model)
         @dummy_model.stub(:versions) { @history }
-        @history.stub(:where).and_return( @history )
+        @history.stub(:where).and_return(@history)
         @history.should_receive(:first).and_return(@preceding_version)
         @preceding_version.should_receive(:next).and_return(@version)
 
@@ -58,7 +105,7 @@ module Historiqueux
 
         subject.should_receive(:fetch_resource).and_return(@dummy_model)
         @dummy_model.stub(:versions) { @history }
-        @history.stub(:where).and_return( @history )
+        @history.stub(:where).and_return(@history)
         @history.should_receive(:first).and_return(@preceding_version)
         @preceding_version.should_receive(:next).and_return(@version)
 
